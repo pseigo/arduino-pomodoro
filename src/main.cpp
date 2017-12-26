6a958333bffca1ef148661f1f7e5cc5b6976d823
Peyton Seigo <pseigo@yahoo.com>
2017-12-26 17:18:44 +0100

    Remove debug output and test functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "TimerOne.h"
 
 SevSeg sevseg; // instantiate a seven segment object
-static const int delayTime = 5;
 static const int debounceDelay = 50;
 int seconds = 60 * 15;
 
@@ -66,27 +65,6 @@ unsigned long button2_pressed_time = 0;
 unsigned long button1_last_debounced_time = 0;
 unsigned long button2_last_debounced_time = 0;
 
-// void checkIncrement(int pin, bool& pressed, int value) {
-//
-//     int reading = digitalRead(pin);
-//
-//     if (reading == HIGH) {
-//         if (!pressed) {
-//             Serial.print("Pin ");
-//             Serial.print(pin);
-//             Serial.print(" ");
-//             Serial.println(value);
-//
-//             pressed = true;
-//
-//             count += value;
-//             sevseg.setNumber(count);
-//         }
-//     } else {
-//         pressed = false;
-//     }
-// }
-
 void tick() {
     tone(pin::audio_ticker, 330, 5);
     seconds--;
@@ -118,62 +96,27 @@ void debounced_press(int pin, int& pressed, unsigned long& pressed_time, unsigne
 
     if (pressed == LOW && pressed_time > 0) {
         int pressed_duration = millis() - pressed_time;
-        // Serial.print("pressed_time: ");
-        // Serial.print(pressed_time);
-        // Serial.print(" | pressed_duration: ");
-        // Serial.println(pressed_duration);
 
         if (pressed_duration < 500) {
             if (value > 0) {
+                // left button
                 Timer1.start();
             } else {
+                // right button
                 Timer1.stop();
             }
-            // sevseg.setNumber(count += value * 5);
         } else if (pressed_duration >= 500 && pressed_duration < 1000) {
-            // sevseg.setNumber(count += value * 50
             Timer1.stop();
         } else {
-            // sevseg.setNumber(count += value * 500);
             Timer1.stop();
         }
-
-        // analogWrite(pin::led_statusR, (count%10)/10.0*255);
-        // analogWrite(pin::led_statusG, (count%100)/10/10.0*255);
-        // analogWrite(pin::led_statusB, (count%1000)/100/10.0*255);
         pressed_time = 0;
     }
 }
 
-void light_show() {
-    static unsigned long timer = millis();
-    static int deciSeconds = 0;
-
-    if (millis() - timer >= delayTime) {
-      timer += delayTime;
-
-      // sevseg.setNumber(count--);
-
-      deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
-
-      if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
-        deciSeconds=0;
-      }
-      analogWrite(pin::led_statusR, (deciSeconds%100)/10/10.0*255);
-      analogWrite(pin::led_statusG, (deciSeconds%1000)/100/10.0*255);
-      analogWrite(pin::led_statusB, (deciSeconds%10000)/1000/10.0*255);
-      // sevseg.setNumber(deciSeconds, 1);
-    }
-}
-
 void loop() {
-    // checkIncrement(pin::in_button1, button1_pressed, 1);
-    // checkIncrement(pin::in_button2, button2_pressed, -1);
-
     debounced_press(pin::in_button1, button1_pressed, button1_pressed_time, button1_last_debounced_time, 1);
     debounced_press(pin::in_button2, button2_pressed, button2_pressed_time, button2_last_debounced_time, -1);
-
-    light_show();
 
     sevseg.refreshDisplay(); // Must run repeatedly
 }
